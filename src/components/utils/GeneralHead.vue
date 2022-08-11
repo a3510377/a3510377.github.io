cc993e6be21194ada925d9b789eb198c91e96f7a
猴子 <a102009102009@gmail.com>
2022-08-11 20:42:50 +0800

    fix: fix seo

---
+++ src/components/utils/GeneralHead.vue
@@ -0,0 +1,51 @@
+<template>
+  <Head>
+    <title>{{ title }}</title>
+    <meta property="og:title" :content="title" />
+    <meta property="og:site_name" :content="title" />
+    <meta property="twitter:title" :content="title" />
+
+    <meta name="description" :content="description" />
+    <meta property="og:description" :content="description" />
+    <meta property="twitter:description" :content="description" />
+
+    <meta name="keywords" :content="keywords" />
+
+    <meta property="og:image" :content="image" />
+    <meta property="og:image:secure_url" :content="image" />
+    <meta property="twitter:image" :content="image" />
+
+    <meta name="theme-color" content="#ffffff" />
+    <meta property="og:url" content="https://mc-cloud-town.github.io" />
+    <meta property="og:type" content="website" />
+    <meta property="twitter:card" content="summary_large_image" />
+
+    <meta name="abstract" content="猴貓 ニャー~~" />
+    <meta name="subject" content="猴貓 ニャー~~" />
+  </Head>
+</template>
+
+<script lang="ts" setup>
+import { Head } from '@vueuse/head';
+
+const props = defineProps<{
+  title?: string;
+  pageTitle?: string;
+  keywords?: string[];
+  description?: string;
+  image?: string;
+}>();
+
+const pageTitle = props.pageTitle || '';
+
+let _title: string | void = void 0;
+if (pageTitle) _title = `猴貓 ニャー~~ | ${pageTitle}`;
+
+const image = props.image || '/logo.png';
+const keywords =
+  props.keywords?.join(',') || '侯寓升,猴貓,猴子,猴子#2027,a3510377';
+
+const description = props.description || '猴子的個人網站-侯寓升';
+
+const title = _title || props.title || '猴貓 ニャー~~';
+</script>
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import store from './stores';
 
 import App from '@/layouts/default.vue';
 import SvgIconComponent from '@/components/utils/SvgIcon.vue';
+import GeneralHeadComponent from '@/components/utils/GeneralHead.vue';
 
 export const createApp = ViteSSG(
   App,
@@ -16,6 +17,7 @@ export const createApp = ViteSSG(
   ({ app }) => {
     app.use(store);
     app.component('SvgIcon', SvgIconComponent);
+    app.component('GeneralHead', GeneralHeadComponent);
   },
   { rootContainer: '#app-mount' }
 );
